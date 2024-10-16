c28cada2be1b3a8ec0baf0a18e13001267f9f809
Enrique Prado <enriquep@avaya.com>
2024-10-15 17:37:38 -0700

    Basic Neo App with SideNav and Header

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { ReactNode } from 'react'
+import './App.css'
+// Neo imports
+import "@avaya/neo-react/avaya-neo-react.css";
+import {
+  AppLayout,
+  Image,
+  LeftNav,
+  TopNav,
+  Widget
+} from "@avaya/neo-react";
+
+function App() {
+  const leftNav: ReactNode = (
+    <LeftNav
+      aria-label="Demo app left nav"
+      currentUrl="http://active.com"
+    >
+      <LeftNav.TopLinkItem
+        icon="interaction-details"
+        label="Workflows"
+        href="/workfloes"
+      />
+      <LeftNav.TopLinkItem icon="user-add" label="Teams" href="/teams" />
+    </LeftNav>
+  );
+
+  const TopNavBar = (
+    <TopNav
+      title="Avaya Workflow"
+      logo={<Image src="/logo.svg" isDecorativeOrBranding />}
+    >
+      <>
+        <TopNav.IconButton aria-label="information" icon="info" />
+        <TopNav.IconButton aria-label="notifications" icon="notifications-on" />
+        <TopNav.IconButton aria-label="Settings" icon="settings" />
+      </>
+    </TopNav>
+  );
+
+  const emptyWidget: ReactNode = (
+    <Widget>
+      <p>Main content goes here</p>
+    </Widget>
+  );
+
+  return (
+    <>
+      <AppLayout
+        header={TopNavBar}
+        leftPanel={leftNav}
+        mainContent={emptyWidget}
+      />
+    </>
+  )
+}
+
+export default App
